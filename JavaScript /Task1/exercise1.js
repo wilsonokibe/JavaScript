011e463038daba87be35329bdba3a19fab631b84
Wilson Okibe <wilson@dealdey.com>
2015-08-05 08:56:44 +0100

    Fix indentataion, include other instances of comparism

--- JavaScript /Task1/exercise1.js
+++ JavaScript /Task1/exercise1.js
@@ -1,30 +1,39 @@
 "use strict";  
-window.onload = function(){  
-
-  let user1 = new User('John', 25);
-  let user2 = new User('Mary', 22);
-
-  const result = user1.compare(user2);       
-}
 
 class User {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  } 
+                 
+  compare(secondUser) { 
+    if (this.age > secondUser.age) {
+      return (this.name + ' is older than ' + secondUser.name+ '\n');
     } 
-                   
-    compare(secondUser) { 
-      if (this.age > secondUser.age) {
-        console.log(this.name + ' is older than ' + secondUser.name);
-      } 
-      else if (this.age < secondUser.age) {
-        console.log(secondUser.name + ' is older than ' + this.name);
-      } 
-      else if (this.age == secondUser.age) {
-        console.log(this.name + ' and ' + secondUser.name + ' are of the same age');
-      } 
-      else {
-        console.log('Nothing to compare');
-      }    
-    }
+    else if (this.age < secondUser.age) {
+      return (this.name + ' is younger than ' + secondUser.name+ '\n');
+      
+    } 
+    else if (this.age == secondUser.age) {
+      return (this.name + ' is same age as ' + secondUser.name+ '\n');
+    } 
+    else {
+      return ("Nothing to compares");
+    }    
   }
+}
+
+let user1 = new User('John', Number('025'));
+let user2 = new User('Mary', Number('22'));
+const result1 = user1.compare(user2); 
+console.log(result1);
+
+let user3 = new User('John', Number('22'));
+let user4 = new User('Mary', Number('22'));
+const result2 = user3.compare(user4); 
+console.log(result2);  
+
+let user5 = new User('John', Number('22'));
+let user6 = new User('Mary', Number('30'));
+const result3 = user5.compare(user6); 
+console.log(result3); 
