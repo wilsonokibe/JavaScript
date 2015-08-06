9e6345d35adcebc720767411d885130a20dfd993
Wilson Okibe <wilson@dealdey.com>
2015-08-06 10:42:17 +0100

    Modify task 12

--- JavaScript /Task12/domain.js
+++ JavaScript /Task12/domain.js
@@ -1,13 +1,8 @@
-"use strict";  
-window.onload = function(){ 
-  const newObject = new Url();
-  newObject.init();
-}
+"use strict"; 
 
 class Url {
   constructor() {}
 
-  //listen to button click
   init() {
     self = this;
     const btn = document.getElementById("myBtn");
@@ -16,32 +11,23 @@ class Url {
     })
   }
 
-  //get inputed value
   getUrl() {
     const urlValue = document.getElementById("urlInput").value;
-    if (urlValue == '') {
-      alert("You have not entered URL.\n Please enter URL.");
-    } else {
+    if (urlValue == '' || urlValue.match(/^ *$/) !== null) {
+      alert(`You have not entered URL.\n Please enter URL.`);
+    } else {      
         let validUrl = this.validateUrl(urlValue);
         if(validUrl) {
           const domainName = this.getDomain(urlValue);
-          //const subDomainName = 
-          //alert("Domain: "+domainName);
           const subDomainName = this.getSubDomain(urlValue);
-          if (subDomainName) {
-            alert("Domain: "+domainName+"\nSub-Domain: "+subDomainName);
-          } 
-            else {
-              alert("Domain: "+domainName);
-            }
-          //alert("Sub-domain: "+domainName);
-        } else {
-          alert("The URL entered is not valid. \nPlease enter valid URL.");
-        }
+          if (subDomainName) { alert(`Domain: ${domainName}\nSub-Domain: ${subDomainName}`); } 
+          else {alert(`Domain: ${domainName}`); }
+        } 
+        else { alert(`The URL entered is not valid. \nPlease enter valid URL.`);}
       }
   }
 
-  //method: validate url entered by user
+  
   validateUrl(urlValue) {
     let urlregex = new RegExp(
       "^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
@@ -51,11 +37,9 @@ class Url {
   getHostName(urlValue) {
     let domainRegex = new RegExp(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
     let match = urlValue.match(domainRegex);
-    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
-      return(match[2]);
-    } else {
-        return null;
-      }
+
+    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) { return(match[2]); } 
+    else { return null; }
   }
 
   getDomain(url) {
@@ -66,9 +50,9 @@ class Url {
       let parts = hostName.split('.').reverse();
         
       if (parts != null && parts.length > 1) {
-        domain = parts[1] + '.' + parts[0];            
+        domain = `${parts[1]}.${parts[0]}`;            
         if (hostName.toLowerCase().indexOf('.co.uk') != -1 && parts.length > 2) {
-          domain = parts[2] + '.' + domain;
+          domain = `${parts[2]}.${domain}`;
         }
       }
     }    
@@ -77,32 +61,21 @@ class Url {
 
   getSubDomain(url) { 
     let subDomain = url;
-    // IF THERE, REMOVE WHITE SPACE FROM BOTH ENDS
     subDomain = subDomain.replace(new RegExp(/^\s+/),""); // START
     subDomain = subDomain.replace(new RegExp(/\s+$/),""); // END
      
-    // IF FOUND, CONVERT BACK SLASHES TO FORWARD SLASHES
-    subDomain = subDomain.replace(new RegExp(/\\/g),"/");
-     
-    // IF THERE, REMOVES 'http://', 'https://' or 'ftp://' FROM THE START
+    subDomain = subDomain.replace(new RegExp(/\\/g),"/"); //REPLACE \ WITH /
+
     subDomain = subDomain.replace(new RegExp(/^http\:\/\/|^https\:\/\/|^ftp\:\/\//i),"");
-     
-    // IF THERE, REMOVES 'www.' FROM THE START OF THE STRING
     subDomain = subDomain.replace(new RegExp(/^www\./i),"");
-     
-    // REMOVE COMPLETE STRING FROM FIRST FORWARD SLASH ON
     subDomain = subDomain.replace(new RegExp(/\/(.*)/),"");
-     
-    // REMOVES '.??.??' OR '.???.??' FROM END - e.g. '.CO.UK', '.COM.AU'
+
     if (subDomain.match(new RegExp(/\.[a-z]{2,3}\.[a-z]{2}$/i))) {
-      subDomain = subDomain.replace(new RegExp(/\.[a-z]{2,3}\.[a-z]{2}$/i),"");
-     
-    // REMOVES '.??' or '.???' or '.????' FROM END - e.g. '.US', '.COM', '.INFO'
+      subDomain = subDomain.replace(new RegExp(/\.[a-z]{2,3}\.[a-z]{2}$/i),"");     
     } else if (subDomain.match(new RegExp(/\.[a-z]{2,4}$/i))) {
       subDomain = subDomain.replace(new RegExp(/\.[a-z]{2,4}$/i),""); 
     }
      
-    // CHECK TO SEE IF THERE IS A DOT '.' LEFT IN THE STRING
     let subDomainStatues = (subDomain.match(new RegExp(/\./g))) ? true : false;
 
     if (subDomainStatues) {
@@ -111,3 +84,6 @@ class Url {
     } else {return null;}     
   }
 }
+
+const newObject = new Url();
+newObject.init();
