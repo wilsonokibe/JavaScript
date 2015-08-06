634ca395991422cb96098f1251f1d845fb7253e1
Wilson Okibe <wilson@dealdey.com>
2015-08-06 09:50:08 +0100

    Separate task 6 and task 10 form validation

--- JavaScript /Task6/formvalidation.js
+++ JavaScript /Task6/formvalidation.js
@@ -1,11 +1,5 @@
 "use strict";  
 
-window.onload = function(){
-  const btn = document.getElementById("myBtn");
-  const formValidation = new Validation(btn);
-  formValidation.init();
-}
-
 class Validation {
   constructor(button) {
     this.button = button;
@@ -13,88 +7,73 @@ class Validation {
 
   init(){
     let self = this;
-    this.button.addEventListener("click", function (){
-      let validationResult = self.getValues();
+    this.button.addEventListener("click", function(event){
+      self.verifyLoginId(event);
+      self.verifyEmail(event);
+      self.verifyName(event);
+      self.verifyHomePage(event);
+      self.verifyAboutMe(event);
     })
   }
 
-  getValues() {
-
-    //GET VALUES OF ELEMENTS IN FORM
+  verifyLoginId(eventValue) {    
     const loginId = document.getElementById("loginId").value;
+    if(loginId === '' || loginId.match(/^ *$/) !== null) {
+      eventValue.preventDefault(); 
+      alert("Login Id cannot be empty.");
+    }
+  }
+
+  verifyEmail(eventValue) {
     const email = document.getElementById("email").value;
-    const name = document.getElementById("name").value;
-    const homePage = document.getElementById("homePage").value; 
-    const aboutMe = document.getElementById("aboutMe").value;
-    const receiveNotification = document.getElementById("receiveNotification").checked;
-
-    //CHECK FOR EMPTY LOGIN INPUT
-    if(loginId == '') { alert("Login Id cannot be empty.");}
-
-    //CHECK FOR EMPTY EMAIL INPUT
-    if(email == '') { 
+    if(email == '' || email.match(/^ *$/) !== null) { 
+      eventValue.preventDefault();
       alert("Email cannot be empty.");
-    } else {
+    } else { }
+  }
 
-      //VALIDATE EMAIL INPUT
-      const validEmail = this.validateEmail(email);
-
-      //IF VALID EMAIL MOVE TO NEXT STATEMENT ELSE ALERT MESSAGE
-      if(validEmail) {} 
-      else {alert("Please enter valid email.");}
+  verifyName(eventValue) {
+    const name = document.getElementById("name").value;
+    if (name == '' || name.match(/^ *$/) !== null) { 
+      eventValue.preventDefault(); 
+      alert("Name cannot be empty.");
     }
+  }
 
-    //CHECK FOR EMPTY NAME INPUT. IF EMPTY ALERT MESSAGE
-    if (name == '') { alert("Name cannot be empty.");}
+  verifyHomePage(eventValue) {
+    const homePage = document.getElementById("homePage").value;
+    if (homePage == '' || homePage.match(/^ *$/) !== null) { 
+      eventValue.preventDefault(); 
+      alert("home page cannot be empty.");
+    } else { }
+  }
 
-    //CHECK FOR EMPTY HOME PAGE INPUT. IF EMPTY ALERT MESSAGE
-    if (homePage == '') { alert("home page cannot be empty.");} 
-    else {
-
-      //VALIDATE HOME PAGE INPUT
-      const validUrl = this.validateUrl(homePage);
-
-      //IF VALID HOME PAGE MOVE TO NEXT STATEMENT ELSE ALERT MESSAGE
-      if(validUrl) {}
-      else { alert("Please enter valid home page.");}
-    }
-
-    //CHECK FOR EMPTY ABOUT ME INPUT. IF EMPTY ALERT MESSAGE
-    if(aboutMe =='') {
+  verifyAboutMe(eventValue) {
+    const aboutMe = document.getElementById("aboutMe").value;
+    if(aboutMe == '' || aboutMe.match(/^ *$/) !== null) {
+      eventValue.preventDefault(); 
       alert("Please tell us aboput yourself.\n This field cannot be empty.")
     } else { 
-
-      //IF VALID 'ABOUT ME' MOVE TO NEXT STATEMENT AND CHECK LENGTH 
       if(aboutMe.length < 50) {
+        eventValue.preventDefault(); 
         alert("Statement about yourself is too short.\n Kindly ensure that you write more than 50 characters of summary on yourself.")
-      } else if (aboutMe.length > 50) {
-
-        //AFTER ALL VALIDATION, CHECK IF CHECKBOX IS CHECKED
-          if(receiveNotification == false) {
-            alert("Receive notification is not checked.");
-          } else if (receiveNotification == true) {
-
-            //iF CHECKED, SUBMIT FORM
-            this.submitForm();
-          }
+      } else if(aboutMe.length > 50) {
+        this.verifyNotification(eventValue);
       }
-   }
-  }
-
-  //VALIDATION METHOD FOR EMAIL
-  validateEmail(email) {
-    const emailRegEx = new RegExp(/^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/gm);
-    return emailRegEx.test(email);
-  }
-
-  //VALIDATION METHOD FOR HOME PAGE
-  validateUrl(urlValue) {
-    let urlregex = new RegExp(
-      "^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
-    return urlregex.test(urlValue);
-  }
-
-  submitForm() {
-    const sumitted = document.getElementById("myForm").submit();
     }
+  }
+
+  verifyNotification(eventValue) {
+    const receiveNotification = document.getElementById("receiveNotification").checked;
+    if(receiveNotification == false) {
+      eventValue.preventDefault(); 
+      alert("Receive notification is not checked.");
+    } else if (receiveNotification == true) {
+      //SUBMITS FORM
+     }
+  }
 }
+
+const btn = document.getElementById("myBtn");
+const formValidation = new Validation(btn);
+formValidation.init();
