cf470ee2b32e808383a125c0648549fed8cdb4c9
Wilson Okibe <wilson@dealdey.com>
2015-08-10 09:06:29 +0100

    Update for task 1

--- JavaScript /Task1/exercise1.js
+++ JavaScript /Task1/exercise1.js
@@ -3,37 +3,39 @@
 class User {
   constructor(name, age) {
     this.name = name;
-    this.age = age;
+    this.age = Number(age);
   } 
                  
   compare(secondUser) { 
+    let stringHolder = '';
     if (this.age > secondUser.age) {
-      return (this.name + ' is older than ' + secondUser.name+ '\n');
+      stringHolder = `${this.name}  is older than  ${secondUser.name}\n`;
     } 
     else if (this.age < secondUser.age) {
-      return (this.name + ' is younger than ' + secondUser.name+ '\n');
+      stringHolder =`${this.name} is younger than ${secondUser.name}\n`;
       
     } 
     else if (this.age == secondUser.age) {
-      return (this.name + ' is same age as ' + secondUser.name+ '\n');
+      stringHolder = `${this.name} is same age as ${secondUser.name}\n`;
     } 
     else {
-      return ("Nothing to compares");
-    }    
+      stringHolder = `Nothing to compare`;
+    } 
+    return stringHolder;   
   }
 }
 
-let user1 = new User('John', Number('025'));
-let user2 = new User('Mary', Number('22'));
+let user1 = new User('John', '025');
+let user2 = new User('Mary', '22');
 const result1 = user1.compare(user2); 
 console.log(result1);
 
-let user3 = new User('John', Number('22'));
-let user4 = new User('Mary', Number('22'));
+let user3 = new User('John', '22');
+let user4 = new User('Mary', '22');
 const result2 = user3.compare(user4); 
 console.log(result2);  
 
-let user5 = new User('John', Number('22'));
-let user6 = new User('Mary', Number('30'));
+let user5 = new User('John', '22');
+let user6 = new User('Mary', '30');
 const result3 = user5.compare(user6); 
 console.log(result3); 
