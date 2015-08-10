d2055983cff57465a8f704149e276f2cdc5ca787
Wilson Okibe <wilson@dealdey.com>
2015-08-10 13:57:18 +0100

    Task 3 update

--- JavaScript /Task3/checkbox3.js
+++ JavaScript /Task3/checkbox3.js
@@ -3,63 +3,56 @@
 class Check {
 
   init() {
-    const daysValue = document.getElementsByClassName("day_class");
-    const noneValue = document.getElementsByClassName("none_class");
+    const daysElement = document.getElementsByClassName("day");
+    const noneElement = document.getElementsByClassName("none");
     let self = this;
 
-    for(let i = 0; i < daysValue.length; i++) {
-      daysValue[i].addEventListener('click', function() {
-        self.uncheckNone();  
-        self.checkDays(i); 
+    //LISTEN TO CLICK ON DAYS CHECKBOX
+    for(let i = 0; i < daysElement.length; i++) {
+      daysElement[i].addEventListener('click', function() {
+        self.unCheckNone(noneElement);  
+        self.checkDaysLimit(i, daysElement); 
       })
     }
 
-    noneValue[0].addEventListener('click', function() {
-      self.checkNone();
+    //LISTEN TO CLICK ON NONE CHECKBOX
+    noneElement[0].addEventListener('click', function() {
+      self.unCheckDays(daysElement);
     })
   }
 
-  checkDays(i) {
-    const daysValue = document.getElementsByClassName("day_class");
-    let count = document.querySelectorAll('input[type="checkbox"]:checked').length;
-    let dayHolder = '';
-
+  checkDaysLimit(i, daysElement) {
+    let count = document.querySelectorAll('input[type="checkbox"]:checked').length; 
     if(count > 3) {
-      daysValue[i].checked = false;
+      daysElement[i].checked = false;
       count -= 1;
-
-      for(let k = 0; k < daysValue.length; k++ ) {
-        if(daysValue[k].checked == true) {
-          dayHolder =  `${dayHolder} ${this.getCheckedDay(k)}`;
-          dayHolder = `${dayHolder},`;
-        }        
-      }
-      alert(`You can only select 3 options.\n You have already selected ${dayHolder}`);
-      } 
+      this.getCheckedDays(daysElement);
+    } 
   }
 
-  uncheckNone() {
-    const noneValue2 = document.getElementsByClassName("none_class");
-    noneValue2[0].checked = false;
+  unCheckNone(noneElement) {
+    noneElement[0].checked = false;
   }
 
-  checkNone() {
-    const days = document.getElementsByClassName("day_class");
-    for(let i = 0; i < days.length; i++) {
-      days[i].checked = false;
+  unCheckDays(daysElement) {
+    for(let i = 0; i < daysElement.length; i++) {
+      daysElement[i].checked = false;
     }    
   }
 
-  getCheckedDay(value) {
-    let day = '';
-    if(value == 0) { day = 'Sunday';} 
-    else if(value == 1) { day = 'Monday';}
-    else if(value == 2) { day = 'Tuesday';}
-    else if(value == 3) { day = 'Wednesday';}
-    else if(value == 4) { day = 'Thursday';}
-    else if(value == 5) { day = 'Friday';}
-    else if(value == 6) { day = 'Saturday';}
-    return day;
+  getCheckedDays(daysElement) {
+    const days = [];
+    for(let k = 0; k < daysElement.length; k++ ) {
+      if(daysElement[k].checked == true) {
+        days.push(daysElement[k].value);
+      }        
+    }
+    this.displayMessage(days);      
+  }
+
+  displayMessage(days) {
+    let dayHolder = days.join(', ');
+    alert(`You can only select 3 options.\n You have already selected ${dayHolder}`);
   }
 }
 
