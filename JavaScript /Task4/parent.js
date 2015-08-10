2d107d7d4e20d110b3c1145fd2dc3104729fb807
Wilson Okibe <wilson@dealdey.com>
2015-08-10 18:28:25 +0100

    Update task 4

--- JavaScript /Task4/parent.js
+++ JavaScript /Task4/parent.js
@@ -4,35 +4,60 @@ class CheckBox {
 
   init() {
     const parentCheckBox = document.querySelectorAll(".parent");
-      this.parentClick(parentCheckBox);
-    
+    this.bindCheckBoxes(parentCheckBox);    
   }
 
-  parentClick(parentCheckBox) {
+  bindCheckBoxes(parentCheckBox) {
+    let self = this;
     for(let i = 0; i < parentCheckBox.length; i++) {     
-      parentCheckBox[i].addEventListener("click", function() {
-        const divElement = document.getElementById(`chckBox${i+1}`);
-        const divChildren = divElement.getElementsByTagName("input");        
-
-        if(parentCheckBox[i].checked == true) {
-          divElement.style.display = "block";
-
-          parentCheckBox[i].scrollIntoView(); 
-
-          for(let k = 0; k < divChildren.length; k++) {
-            if(divChildren[k].type == 'checkbox') {
-              divChildren[k].checked = true;
-            } 
-          }
-        } else if(parentCheckBox[i].checked == false) {
-          for(let x = 0; x < divChildren.length; x++) { 
-            divChildren[x].checked = false; 
-          }
-          divElement.style.display = "none";
-        }
+      parentCheckBox[i].addEventListener("click", function() {         
+        self.hideOrSHowChildren(i, parentCheckBox) ;        
       })
     }
+  }  
+
+  hideOrSHowChildren(i, parentCheckBox) {
+    let self = this;
+    self.checkParent(parentCheckBox, i)
   }
+
+  checkParent(parentCheckBox, i) {
+    let self = this; 
+    let value = true;   
+
+    if(parentCheckBox[i].checked == true) {
+      let show = "block";
+      self.actionForChecked(value, show, i);
+      parentCheckBox[i].scrollIntoView();
+    }else {
+      let show = "none";
+      let value = false;
+      self.actionForChecked(value, show, i);
+    }
+  }
+
+  actionForChecked(value, show, i) {
+    let self = this; 
+    const divElement = self.getParentCheckBox(i); 
+    const divChildren = self.getChildrenCheckBox(divElement); 
+    divElement.style.display = show;
+    
+    for(let k = 0; k < divChildren.length; k++) {
+      if(divChildren[k].type == 'checkbox') {
+        divChildren[k].checked = value;
+      } 
+    }
+  }
+
+  getParentCheckBox(i) {
+    const divElement = document.getElementById(`chckBox${i+1}`);
+    return divElement;
+  }
+
+  getChildrenCheckBox(divElement) {    
+    const divChildren = divElement.getElementsByTagName("input"); 
+    return divChildren;
+  }  
 }
 
 const newParentChild = new CheckBox();
