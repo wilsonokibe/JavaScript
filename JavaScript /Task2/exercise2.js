2e18eefe5fbe36e526431d618ebf45d7af3ad0e8
Wilson Okibe <wilson@dealdey.com>
2015-08-10 09:29:01 +0100

    Update for task 2

--- JavaScript /Task2/exercise2.js
+++ JavaScript /Task2/exercise2.js
@@ -1,22 +1,20 @@
 "use strict";
 
 class Check {
-  constructor() {}  
-  
-  //CHECKS ALL CHECKBOXES IF VALUE IS 1 VICE-VERSA                 
-  selectToggle(toggle) {
+                 
+  selectToggle(status) {
     const colors = document.getElementsByTagName("input");
     let counter = 0;
     for(counter; counter < colors.length; counter++) {
-      colors[counter].checked = toggle;
+      colors[counter].checked = status;
     }
   }  
 
   init() {
     const all = document.getElementById("all");
     const none = document.getElementById("none");
-
     let self = this;
+
     all.addEventListener('click', function() {
       self.selectToggle(true);
     })
