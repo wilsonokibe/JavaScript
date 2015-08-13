0d17958cdeecff2e9e6afe88898299a7663ddf78
Wilson Okibe <wilson@dealdey.com>
2015-08-13 13:55:00 +0100

    Assign method for empty fields validation. Use descriptive variable names

--- JavaScript /Task8/url_task.js
+++ JavaScript /Task8/url_task.js
@@ -1,34 +1,44 @@
 "use strict";  
 
-class Url {
+class CheckUrl {
 
   init () {
-    let newUrl = this.retrieveUrl();
-    let popPage = window.open(newUrl, "_blank", "height = 450, width=400, top=10, left=10, resizable=no, scrollbars=no, status=no, toolbar=no, menubar=no");
+    let inputedUrl = this.retrieveUrl();
+    window.open(inputedUrl, "_blank", "height = 450, width=400, top=10, left=10, resizable=no, scrollbars=no, status=no, toolbar=no, menubar=no");
   }
 
   retrieveUrl() {
-    let userUrl = prompt("Please input url");
-    while(userUrl == '' || userUrl.match(/^ *$/) !== null) {
-      userUrl = prompt("You have not entered URL. \nPlease enter URL:");  
-    }
+    let promptResult = prompt("Please input url");
+    
+    this.checkEmptyField(promptResult);
+    let isUrl = this.validateUrl(promptResult);
 
-    let verifyUrl = this.validateUrl(userUrl);
-    while(!verifyUrl) {
-      userUrl = prompt("You have entered an INVALID URL. \nPlease enter valid URL:");
-      if(userUrl != '') {
-        verifyUrl = this.validateUrl(userUrl);
+    while(!isUrl) {
+      promptResult = prompt("You have entered an INVALID URL. \nPlease enter valid URL:");
+      this.checkEmptyField(promptResult);
+      if(promptResult != '') {        
+        isUrl = this.validateUrl(promptResult);
       }      
     }
-    return userUrl;
+    return promptResult;
   }
 
-  validateUrl(urlValue) {
-    var urlregex = new RegExp(
+  checkEmptyField(promptResult) {
+    if(promptResult == null) 
+    { alert("You may NOT cancel.")  }
+    else {
+      while(promptResult == '' || promptResult.match(/^ *$/) !== null) {
+        promptResult = prompt("You have not entered URL. \nPlease enter URL:");
+      }
+    }
+  }
+
+  validateUrl(url) {
+    let urlregex = new RegExp(
       "^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
-    return urlregex.test(urlValue);
+    return urlregex.test(url);
   }
 }
 
-const names = new Url();
-names.init();
+const newCheckUrlObject = new CheckUrl();
+newCheckUrlObject.init();
