9ef85519e319fc082d8624f5a0c22a9467ac315e
Wilson Okibe <wilson@dealdey.com>
2015-08-13 15:47:57 +0100

    Update task 9

--- JavaScript /Task9/country.js
+++ JavaScript /Task9/country.js
@@ -9,42 +9,30 @@ class Country{
   init() {
     let self = this;
     const addSelectOption = document.getElementById("add_country");
-    const removeSelectOption = document.getElementById("remove_country");    
+    const removeSelectOption = document.getElementById("remove_country");
+
     addBtn.addEventListener("click", function () {
-      self.addOption(addSelectOption, removeSelectOption);     
+      self.swapOption(addSelectOption, removeSelectOption);     
     });
 
     removeBtn.addEventListener("click", function () {
-      self.removeOption(addSelectOption, removeSelectOption);     
+      self.swapOption(removeSelectOption, addSelectOption);     
     });
   }
 
-  addOption(addSelectOption, removeSelectOption) {  
+  swapOption(selectOption1, selectOption2) {  
     let selectId = '';
     let selectText = '';
 
-    for(let i = addSelectOption.options.length-1; i >= 0; i--) {
-      if (addSelectOption.options[i].selected == true) {
-        selectId = addSelectOption.options[i].value;
-        selectText = addSelectOption.options[i].text;
-        let newRow = new Option(selectText, selectId);
-        removeSelectOption.options[removeSelectOption.length] = newRow;
-        addSelectOption.options[i] = null;
-      }
-    }
-  }
+    for(let i = selectOption1.options.length-1; i >= 0; i--) {
+      if (selectOption1.options[i].selected == true) {
+        selectId = selectOption1.options[i].value;
+        selectText = selectOption1.options[i].text;
 
-  removeOption(addSelectOption, removeSelectOption) {  
-    let selectId = '';
-    let selectText = '';
-
-    for(let i = removeSelectOption.options.length-1; i >= 0; i--) {
-      if(removeSelectOption.options[i].selected == true) {
-        selectId = removeSelectOption.options[i].value;
-        selectText = removeSelectOption.options[i].text;
         let newRow = new Option(selectText, selectId);
-        addSelectOption.options[addSelectOption.options.length] = newRow;
-        removeSelectOption.options[i] = null; 
+        
+        selectOption2.options[selectOption2.length] = newRow;
+        selectOption1.options[i] = null;
       }
     }
   }
