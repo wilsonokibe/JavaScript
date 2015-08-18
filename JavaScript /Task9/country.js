8fd8f4ffeab5c83e025eed7f93a70a5ba1b8d74f
Wilson Okibe <wilson@dealdey.com>
2015-08-18 09:41:11 +0100

    Modularise Code

--- JavaScript /Task9/country.js
+++ JavaScript /Task9/country.js
@@ -12,28 +12,18 @@ class Country{
     const removeSelectOption = document.getElementById("remove_country");
 
     addBtn.addEventListener("click", function () {
-      self.swapOption(addSelectOption, removeSelectOption);     
+      self.changeOption(addSelectOption, removeSelectOption);     
     });
 
     removeBtn.addEventListener("click", function () {
-      self.swapOption(removeSelectOption, addSelectOption);     
+      self.changeOption(removeSelectOption, addSelectOption);     
     });
   }
 
-  swapOption(selectOption1, selectOption2) {  
-    let selectId = '';
-    let selectText = '';
-
-    for(let i = selectOption1.options.length-1; i >= 0; i--) {
-      if (selectOption1.options[i].selected == true) {
-        selectId = selectOption1.options[i].value;
-        selectText = selectOption1.options[i].text;
-
-        let newRow = new Option(selectText, selectId);
-        
-        selectOption2.options[selectOption2.length] = newRow;
-        selectOption1.options[i] = null;
-      }
+  changeOption(selectOption1, selectOption2) {
+    let options = selectOption1.selectedOptions;
+    for(let i = options.length-1; i >= 0; i--) {
+      selectOption2.appendChild(options[i]);
     }
   }
 }
