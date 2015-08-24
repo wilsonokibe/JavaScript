280ed9401cf87a31debcc50cc53600a1ae379cab
Wilson Okibe <wilson@dealdey.com>
2015-08-24 19:05:03 +0100

    Reduce parameters

--- JavaScript /Task5/table.js
+++ JavaScript /Task5/table.js
@@ -6,49 +6,46 @@ class Table {
   }
 
   init() {
-    let self = this;
     this.createTableHeader();
   }
 
   createTableHeader() {
-    const centre = "centre";
     const nameField = this.createHeaderText("Name");
     const emailField = this.createHeaderText("Email");
     const addRowButton = this.createButton("Add Row");
     
     const table = document.createElement("table");
-    const tableHead = table.createTHead(); 
-    table.setAttribute("class", centre);
-    table.appendChild(tableHead);
-    this.createNewRow(tableHead);
-    this.appendRow(tableHead, nameField, emailField, addRowButton);
+    this.tableHead = table.createTHead(); 
+    table.setAttribute("class", "centre");
+    table.appendChild(this.tableHead);
+    this.createNewRow();
+    this.appendRow(nameField, emailField, addRowButton);
 
     //APPEND TABLE TO DOCUMENT BODY
     container.insertBefore(table, container.childNodes[0]); 
     newTableButton.style.display = "none";
 
-    this.addRowClick(tableHead, addRowButton);
+    this.addRowClick(addRowButton);
   }
 
-  addRowClick(tableHead, addRowButton) {
+  addRowClick(addRowButton) {
     let self = this;
     addRowButton.addEventListener("click", function() {
       const nameInput = self.createInputField();
       const emailInput = self.createInputField();
       const saveRowButton = self.createButton("Save");
-      self.createNewRow(tableHead);
-      self.appendRow(tableHead, nameInput, emailInput, saveRowButton);
-
-      let rowCount = tableHead.rows.length;
+      self.createNewRow();
+      self.appendRow(nameInput, emailInput, saveRowButton);
       self.buttonVisibility("none", addRowButton);
-      self.saveRow(tableHead, rowCount, nameInput, emailInput, saveRowButton, addRowButton);
+      self.saveRow(nameInput, emailInput, saveRowButton, addRowButton);
     })
   }
 
-  saveRow(table, rowCount, nameInput, emailInput, saveButton, addRowButton) {
+  saveRow(nameInput, emailInput, saveButton, addRowButton) {
     let self = this;
+    let rowCount = self.tableHead.rows.length
     saveButton.addEventListener("click", function() { 
-      let row = table.rows[rowCount-1]; 
+      let row = self.tableHead.rows[rowCount-1]; 
       let nameText = self.nameText(nameInput.value);     
       let emailText = self.nameText(emailInput.value);
 
@@ -57,17 +54,17 @@ class Table {
           row.cells[0].replaceChild(nameText, nameInput);
           row.cells[1].replaceChild(emailText, emailInput);      
           self.buttonVisibility("block", addRowButton);
-          self.replaceSaveButtons(table, rowCount-1, this, nameText, emailText, addRowButton);          
+          self.replaceSaveButtons(this, nameText, emailText, addRowButton);          
         }
       }
     })
   }
 
   validateEmptyInputField(value, fieldName) {
-    if(value == '' || value.match(/^ *$/) !== null) {
+    if(value == null || value.trim() == '') {
       alert(`Please enter valid ${fieldName} field`);
       return false;
-    } else {  return true  };
+    } else {return true;};
   }
 
   validateNameInputField(nameInput) {
@@ -78,81 +75,83 @@ class Table {
         alert(`Please enter valid name format`);
         return false; 
       }
-    } else {  return result;  }
+    } else {return result;}
   }
 
   validateEmailInputField(emailInput, eventValue) {
     let result = this.validateEmptyInputField(emailInput.value, "email");
     if(result) {
-      if(emailInput.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {  return true } 
+      if(emailInput.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {return true;} 
       else {
         alert(`Please enter valid email format`);
         return false;
       }
       return true;
-    } else {  return result;  }
+    } else {return result;}
   }
 
-  replaceSaveButtons(table, rowCount, saveRow, nameText, emailText, addRowButton) {    
+  replaceSaveButtons(saveRow, nameText, emailText, addRowButton) {    
     let self = this;
-    const editButton = self.createButton("Edit");
-    const deleteButton = self.createButton("Delete");
-    editButton.setAttribute("class", rowCount);
-    deleteButton.setAttribute("class", rowCount);
-    table.rows[rowCount].cells[2].replaceChild(editButton, saveRow);
-    table.rows[rowCount].cells[2].appendChild(deleteButton);
-    self.deleteClick(deleteButton, table);
-    self.switchToEditMode(editButton, table, nameText, emailText, addRowButton, deleteButton);
+    let row = self.tableHead.rows.length;
+    this.editButton = self.createButton("Edit");
+    this.deleteButton = self.createButton("Delete");
+    this.editButton.setAttribute("class", row-1);
+    this.deleteButton.setAttribute("class", row-1);
+    this.tableHead.rows[row-1].cells[2].replaceChild(this.editButton, saveRow);
+    this.tableHead.rows[row-1].cells[2].appendChild(this.deleteButton);
+    self.deleteClick(this.deleteButton);
+    self.switchToEditMode(nameText, emailText, addRowButton);
   }
 
-  switchToEditMode(editButton, table, nameText, emailText, addRowButton, deleteButton) {
+  switchToEditMode(nameText, emailText, addRowButton) {
     let self = this;
-    let buttonClass = editButton.className;
-    let nameValue = table.rows[buttonClass].cells[0].innerHTML;
-    let emailValue = table.rows[buttonClass].cells[1].innerHTML;
+    let buttonClass = this.editButton.className;
+    let nameValue = this.tableHead.rows[buttonClass].cells[0].innerHTML;
+    let emailValue = this.tableHead.rows[buttonClass].cells[1].innerHTML;
     let nameInput = this.createInputField();
     let emailInput = this.createInputField();
-    editButton.addEventListener("click", function() {     
+    this.editButton.addEventListener("click", function() {     
       const saveButtonInsideEdit = self.createButton("Save");     
       self.buttonVisibility("none", addRowButton);
-      self.buttonVisibility("none", editButton);
-      self.buttonVisibility("none", deleteButton);
+      self.buttonVisibility("none", self.editButton);
+      self.buttonVisibility("none", self.deleteButton);
       
       buttonClass = Number(buttonClass);      
       nameInput.value = nameValue;
       emailInput.value = emailValue;
 
       //SWITCH FROM LABEL TO INPUT
-      self.switchToTextField(table, nameInput, nameText, buttonClass, 0);
-      self.switchToTextField(table, emailInput, emailText, buttonClass, 1);
-      table.rows[buttonClass].cells[2].appendChild(saveButtonInsideEdit);
+      self.switchToTextField(nameInput, nameText, buttonClass, 0);
+      self.switchToTextField(emailInput, emailText, buttonClass, 1);
+      self.tableHead.rows[buttonClass].cells[2].appendChild(saveButtonInsideEdit);
 
       //validate input on save click
       buttonClass += 1;      
-      self.saveRow(table, buttonClass, nameInput, emailInput, saveButtonInsideEdit, addRowButton);
+      self.saveRow(nameInput, emailInput, saveButtonInsideEdit, addRowButton);
     });
   }
 
-  switchToTextField(table, InputField, TextField, buttonClass, index) {    
-    table.rows[buttonClass].cells[index].replaceChild(InputField, TextField);
+  switchToTextField(InputField, TextField, buttonClass, index) {    
+    this.tableHead.rows[buttonClass].cells[index].replaceChild(InputField, TextField);
   }
 
-  deleteClick(deleteButton, table) {
-    deleteButton.addEventListener("click", function(){      
+  deleteClick() {
+    let self = this;
+    this.deleteButton.addEventListener("click", function(){      
       let deleteConfirm = confirm("Are you sure?");
       if(deleteConfirm){
-        var row = deleteButton.parentNode.parentNode;
+        var row = self.deleteButton.parentNode.parentNode;
         row.parentNode.removeChild(row);
       }        
     })
   }
 
-  createNewRow(tableHead) {
-    const rowCount = tableHead.rows.length;
-    let row = tableHead.insertRow(rowCount);
-    let nameField = tableHead.rows[rowCount].insertCell(0);
-    let emailField = tableHead.rows[rowCount].insertCell(1);
-    tableHead.rows[rowCount].insertCell(2); 
+  createNewRow() {
+    const rowCount = this.tableHead.rows.length;
+    let row = this.tableHead.insertRow(rowCount);
+    let nameField = this.tableHead.rows[rowCount].insertCell(0);
+    let emailField = this.tableHead.rows[rowCount].insertCell(1);
+    this.tableHead.rows[rowCount].insertCell(2); 
     row.setAttribute("class", rowCount);  
     nameField.setAttribute("class", "name");
     emailField.setAttribute("class", "email");
@@ -169,12 +168,11 @@ class Table {
     return result;
   }
 
-  appendRow(tableHead, nameField, emailField, addRowButton) {
-    let self = this;
-    const rowCount = tableHead.rows.length;
-    tableHead.rows[rowCount-1].cells[0].appendChild(nameField);
-    tableHead.rows[rowCount-1].cells[1].appendChild(emailField);    
-    tableHead.rows[rowCount-1].cells[2].appendChild(addRowButton);
+  appendRow(nameField, emailField, addRowButton) {
+    const rowCount = this.tableHead.rows.length;
+    this.tableHead.rows[rowCount-1].cells[0].appendChild(nameField);
+    this.tableHead.rows[rowCount-1].cells[1].appendChild(emailField);    
+    this.tableHead.rows[rowCount-1].cells[2].appendChild(addRowButton);
   }
 
   createButton(buttonText) {
